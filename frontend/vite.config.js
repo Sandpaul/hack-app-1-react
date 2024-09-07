1f7e42de6daa9864ce975ff7f55b2b3fecc1ce14
Sandpaul <paul_sandford@hotmail.co.uk>
2024-09-07 19:52:06 +0100

    add outDir of 'build' to vite.config.js.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build',
+  },
 })
+
+// // vite.config.js
+// export default {
+//   build: {
+//     outDir: 'build', // Changes output folder from 'dist' to 'build'
+//   },
+// };
