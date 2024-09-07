d975858887dc35ab7c6e1fa6f8023dbf9ed73be8
Sandpaul <paul_sandford@hotmail.co.uk>
2024-09-07 22:56:37 +0100

    remove console.logs

--- backend/src/fetchImage.mjs
+++ backend/src/fetchImage.mjs
@@ -3,14 +3,11 @@ import fetch from 'node-fetch'
 const fetchImage = async (imageUrl) => {
     try {
       const response = await fetch(imageUrl)
-      console.log(response);
       
       if (!response.ok) {
         throw new Error(`Failed to fetch image: ${response.statusText}`)
       }
       const imageBuffer = await response.arrayBuffer()
-      console.log('>>>>>', imageBuffer, '<<<<<');
-      // console.log('>>>>>', imageBuffer.toString('base64'), '<<<<<');
       
       
       return imageBuffer
