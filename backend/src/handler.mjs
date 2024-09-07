ab49539c892f35a05d7802207b5cead0a93fcc35
Sandpaul <paul_sandford@hotmail.co.uk>
2024-09-07 22:44:04 +0100

    build readTextFromImage in backend.

--- backend/src/handler.mjs
+++ backend/src/handler.mjs
@@ -1,5 +1,8 @@
 'use strict';
 import fetchImage from "./fetchImage.mjs";
+import { TextractClient, DetectDocumentTextCommand } from '@aws-sdk/client-textract'
+
+const textractClient = new TextractClient({region: 'eu-west-1'})
 
 export const handle = async (event, context) => {
   return {
@@ -29,6 +32,43 @@ export const getImage = async (event) => {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({ error: 'Failed to fetch image' }),
+    }
+  }
+}
+
+export const readTextFromImage = async (event) => {
+  const imageUrl = 'https://gradacademydatastore.blob.core.windows.net/gradacademydatastore/sinclair/735ac977-ed49-4411-87a4-533124806170jpeg'
+  try {
+    const imageBase64 = await fetchImage(imageUrl)
+    const imageBytes = Buffer.from(imageBase64, 'base64')
+    
+    const command = new DetectDocumentTextCommand({
+      Document: {
+        Bytes: imageBytes,
+      }
+    })
+    const response = await textractClient.send(command)
+    const blocks = response.Blocks || []
+    const text = blocks
+      .filter(block => block.BlockType === 'LINE')
+      .map(block => block.Text)
+      .join('/n')
+    
+    return {
+      statusCode: 200,
+      body: JSON.stringify({text}),
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    }
+  } catch (error) {
+    console.error('Error processing image:', error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: `Failed to process image: ${error.message}` }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
     }
   }
 }
